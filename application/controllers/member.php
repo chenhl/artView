a7700ae581bd8b7409b24246a87b648789e84f69
Administrator <baby@baby.com>
2018-11-02 13:50:33 +0800

    update

--- application/controllers/member.php
+++ application/controllers/member.php
@@ -8,14 +8,58 @@ class Member extends Base_Controller {
         parent::__construct();
         $this->load->model(array('article', 'channel'));
     }
+
     /**
      * 首页
      */
     public function index() {
         $this->display('wap/usercenter_new.html');
     }
+
     /**
-     * 登录显示页
+     * 系统消息 列表
+     * @param type $param
+     */
+    public function message() {
+        $this->display('wap/uc_message.html');
+    }
+
+    /**
+     * 我的关注（人）
+     * @param type $param
+     */
+    public function follow() {
+
+        $this->display('wap/uc_follow.html');
+    }
+
+    /**
+     * 我的收藏（新闻）列表
+     * @param type $param
+     */
+    public function collect() {
+
+        $this->display('wap/uc_collect.html');
+    }
+
+    /**
+     * 我的反馈（列表）
+     * @param type $param
+     */
+    public function feedback() {
+        
+    }
+
+    /**
+     * 我的反馈（添加）
+     * @param type $param
+     */
+    public function addback() {
+        
+    }
+
+    /**
+     * 登录显示页，全部依赖第三方qq、wechat
      * 
      */
     public function login() {
@@ -31,11 +75,8 @@ class Member extends Base_Controller {
         }
         $channel = $this->channel->getList($param);
         $this->assign("channels", $channel);
-
 //        $this->assign("article_list", $data);
-
         $this->display('wap/login.html');
     }
 
-    
 }
