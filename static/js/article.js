b89cfe3cc53f2ed944a8f2112f21fdb5d902f478
Administrator <cxo114.com@163.com>
2018-11-12 23:06:59 +0800

    update

--- static/js/article.js
+++ static/js/article.js
@@ -4,6 +4,7 @@ var domDown = {
     domLoad: '<div class="dropload-load"><div class="k-line k-line-1"></div><div class="k-line k-line-2"></div><div class="k-line k-line-3"></div><div class="k-line k-line-4"></div></div>',
     domNoData: '<div class="dropload-noData"></div>'
 };
+//ajax防重点击锁
 var hasClick = false;
 
 function showMsg() {
