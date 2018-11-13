0246ec70f5e143b77584836335c4a47736d855b8
Administrator <baby@baby.com>
2018-11-13 13:23:17 +0800

    update

--- application/config/oauth.php
+++ application/config/oauth.php
@@ -14,23 +14,23 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 
 $config['oauth_qq'] = array(
-    'appid' => $_SERVER['QQ_APPID'],
-    'appkey' => $_SERVER['QQ_APPKEY'],
+    'appid' => trim($_SERVER['QQ_APPID']),
+    'appkey' => trim($_SERVER['QQ_APPKEY']),
     'callbackUrl' => APP_BASE_URL.'/member/oauth/qq',
 );
 $config['oauth_weibo'] = array(
-    'appid' => $_SERVER['WEIBO_APPID'],
-    'appkey' => $_SERVER['WEIBO_APPKEY'],
+    'appid' => trim($_SERVER['WEIBO_APPID']),
+    'appkey' => trim($_SERVER['WEIBO_APPKEY']),
     'callbackUrl' => APP_BASE_URL.'/member/oauth/weibo',
 );
 $config['oauth_weixin'] = array(
-    'appid' => $_SERVER['WEIXIN_APPID'],
-    'appkey' => $_SERVER['WEIXIN_APPKEY'],
+    'appid' => trim($_SERVER['WEIXIN_APPID']),
+    'appkey' => trim($_SERVER['WEIXIN_APPKEY']),
     'callbackUrl' => APP_BASE_URL.'/member/oauth/weixin',
 );
 $config['oauth_baidu'] = array(
-    'appid' => $_SERVER['BAIDU_APPID'],
-    'appkey' => $_SERVER['BAIDU_APPKEY'],
+    'appid' => trim($_SERVER['BAIDU_APPID']),
+    'appkey' => trim($_SERVER['BAIDU_APPKEY']),
     'callbackUrl' => APP_BASE_URL.'/member/oauth/baidu',
 );
 
